32c71fe07603c3400b55ab9387074420cd771ba4
Daniel <benjamindaniel706@gmail.com>
2024-12-12 09:44:16 +0100

    chore: update readme

--- frontend/src/sections/pending.tsx
+++ frontend/src/sections/pending.tsx
@@ -2,7 +2,7 @@ import { Loading } from "@/components/loading";
 
 export function PendingSection() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center mt-[190px]">
       <Loading />
     </div>
   );
